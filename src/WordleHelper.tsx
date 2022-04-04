466ebc5c5cb7f90c6f1a0c71e28c4e552e2b86e7
jyi-jakubgvogel <kubi.vogel7@hotmail.com>
2022-04-04 00:07:23 -0400

    Style background and create header

---
+++ src/WordleHelper.tsx
@@ -0,0 +1,23 @@
+import './WordleHelper.css';
+
+const WordleHelper = () => {
+  return (
+    <div className="background">
+      <div className="main">
+        <div className="heading">
+          <div className="help">Help</div>
+          <div className="title">Wordle Helper</div>
+          <div className="reset">
+            Reset { /* Placeholder for reset icon */ }
+          </div>
+        </div>  
+        <div className="helper">
+          { /* <Helper /> */ }
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default WordleHelper;
